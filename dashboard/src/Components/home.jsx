ca88010a19d313ad844ab26147288331cf9c27bd
Kkkermit <jeaton446@gmail.com>
2024-02-03 22:41:22 +0000

    add: Added a horizontal scroll bar for the review section and added the review section in

--- dashboard/src/Components/home.jsx
+++ dashboard/src/Components/home.jsx
@@ -7,23 +7,42 @@ import Selling from '../Assets/selling.svg';
 import Products from '../Assets/products.svg';
 import Pricing from '../Assets/pricing.svg';
 import Platforms from '../Assets/platforms.svg';
-import Shipping from '../Assets/shipping.svg'
-import Customer from '../Assets/customer.svg'
+import Shipping from '../Assets/shipping.svg';
+import Customer from '../Assets/customer.svg';
+import EbayLogo from '../Assets/ebayLogo.svg';
 
+// Header info //
 const Header = config.header;
 const Para = config.paragraph;
+const ReviewHeader = config.reviewHeader
+// Selling info //
 const Intro = config.selling.intro;
 const SellingPara = config.selling.sellingPara;
+// Products info //
 const PIntro = config.products.intro;
 const ProductPara = config.products.productPara;
+// Pricing info //
 const PrIntro = config.pricing.intro;
 const PricingPara = config.pricing.pricingPara;
+// Platforms info //
 const PlIntro = config.platforms.intro;
 const PlatformPara = config.platforms.platformPara;
+// Shipping info //
 const SIntro = config.shipping.intro
 const ShippingPara = config.shipping.shippingPara;
+// Customer info //
 const CIntro = config.customer.intro;
 const CustomerPara = config.customer.customerPara;
+// Reviews info //
+const RIntro1 = config.ebayReviews.intro1;
+const Reviews1 = config.ebayReviews.review1;
+const RIntro2 = config.ebayReviews.intro2;
+const Reviews2 = config.ebayReviews.review2;
+const RIntro3 = config.ebayReviews.intro3;
+const Reviews3 = config.ebayReviews.review3;
+const RIntro4 = config.ebayReviews.intro4;
+const Reviews4 = config.ebayReviews.review4;
+
 
 function Home() {
     const [isLoading, setIsLoading] = useState(true);
@@ -133,6 +152,62 @@ function Home() {
                 </div>
             </div>
             </div>
+            <div className='home-blank-container'>
+                <div className='home-blank-doc'>
+                    <header id='home-blank-doc'></header>
+                </div>
+            </div>
+            <div className='home-para-container'>
+                <header id='home-para'>{ReviewHeader}</header>
+            </div>
+            <ul className='has-scrollbar'>
+            <div className='home-reviews-container'>
+                <div className='home-reviews'>
+                    <article className='home-reviews-article-1'>
+                        <div className='home-reviews-img'>
+                            <img id='home-reviews-img' src={EbayLogo} alt="Ebay Logo" />
+                        </div>
+                        <div className='home-reviews-box'>
+                            <h1 className='home-art-h1'>{RIntro1}</h1>
+                            <h2 className='home-art-h2'>{Reviews1}</h2>
+                        </div>
+                    </article>
+                </div>
+                <div className='home-reviews'>
+                    <article className='home-reviews-article-2'>
+                        <div className='home-reviews-img'>
+                            <img id='home-reviews-img' src={EbayLogo} alt="Ebay Logo" />
+                        </div>
+                        <div className='home-reviews-box'>
+                            <h1 className='home-art-h1'>{RIntro2}</h1>
+                            <h2 className='home-art-h2'>{Reviews2}</h2>
+                        </div>
+                    </article>
+                </div>
+                <div className='home-reviews'>
+                    <article className='home-reviews-article-3'>
+                        <div className='home-reviews-img'>
+                            <img id='home-reviews-img' src={EbayLogo} alt="Ebay Logo" />
+                        </div>
+                        <div className='home-reviews-box'>
+                            <h1 className='home-art-h1'>{RIntro3}</h1>
+                            <h2 className='home-art-h2'>{Reviews3}</h2> 
+                        </div>
+                    </article>
+                </div>
+                <div className='home-reviews'>
+                    <article className='home-reviews-article-4'>
+                        <div className='home-reviews-img'>
+                            <img id='home-reviews-img' src={EbayLogo} alt="Ebay Logo" />
+                        </div>
+                        <div className='home-reviews-box'>
+                            <h1 className='home-art-h1'>{RIntro4}</h1>
+                            <h2 className='home-art-h2'>{Reviews4}</h2>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            </ul>
         </div>
         }
         </>
